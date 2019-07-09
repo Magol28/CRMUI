39ccf2043d795979d73d181116d587efd2b909d1
Miguel <magol@hotmail.es>
2019-07-09 16:00:15 -0500

    modulo_CPS: creacion del servicio

--- src/app/main/apps/security/services/resource.service.ts
+++ src/app/main/apps/security/services/resource.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders, HttpParams  } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Resource } from 'app/models/Resource';
 @Injectable({
   providedIn: 'root'
 })
 export class ResourceService {
   url = 'http://3.80.207.211:8081/resource/getAll';
   constructor(public http: HttpClient) { }
-  getAll(): Observable<any> {
-    return this.http.get(this.url);
+  getAll(): Observable<Resource[]> {
+    return this.http.get<Resource[]>(this.url);
   }
   }
 
