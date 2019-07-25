1d03eb369614827f9c71a133dc7ec4364d24fc71
Marcelo <marcelo96mh@hotmail.com>
2019-07-25 01:07:48 -0500

    email
    email

--- src/app/main/apps/marketing/state-campaing-id/state-campaing-id.component.ts
+++ src/app/main/apps/marketing/state-campaing-id/state-campaing-id.component.ts
@@ -25,6 +25,10 @@ export class StateCampaingIdComponent implements OnInit {
     resources: any[];
     selectedOptions = [];
     selectedOption;
+    flatCampaign: boolean = false;
+    flatEmail: boolean = false;
+    flatCampaignError: boolean = false;
+    flatEmailError: boolean = false;
 
     constructor(
         private activateR: ActivatedRoute,
@@ -104,14 +108,40 @@ export class StateCampaingIdComponent implements OnInit {
         this.selectedOption = $event;
     }
     sendEmail(): void {
-        this._campaing.getEmail(this.form.value.id).subscribe(data => {});
-        alert("Campaign Send Email");
+        this._campaing.getEmail(this.form.value.id).subscribe(
+            data => {
+                this.flatEmail = true;
+                this.flatEmailError = false;
+
+                this.flatCampaign = false;
+                this.flatCampaignError = false;
+            },
+            error => {
+                this.flatEmail = false;
+                this.flatEmailError = true;
+
+                this.flatCampaign = false;
+                this.flatCampaignError = false;
+            }
+        );
     }
 
     updateState(): void {
-        this._campaing
-            .putStage(this.form.value, this.selected)
-            .subscribe(data => {});
-        alert("Modified Campaign Stage");
+        this._campaing.putStage(this.form.value, this.selected).subscribe(
+            data => {
+                this.flatCampaign = true;
+                this.flatCampaignError = false;
+
+                this.flatEmail = false;
+                this.flatEmailError = false;
+            },
+            error => {
+                this.flatCampaign = false;
+                this.flatCampaignError = true;
+
+                this.flatEmail = false;
+                this.flatEmailError = false;
+            }
+        );
     }
 }
