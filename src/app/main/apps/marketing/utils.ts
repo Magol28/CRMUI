75c86fd22fb9c6adf1cd7964389cedca5c6f6a10
Miguel <magol@hotmail.es>
2019-07-25 16:51:31 -0500

    marketing

--- src/app/main/apps/marketing/utils.ts
+++ src/app/main/apps/marketing/utils.ts
@@ -1,5 +1,5 @@
 var ipOracle = "http://3.91.68.253:8000/service/";
 
-var ipProducts = "http://3.91.68.253:5002/api/";
+var ipProducts = "http://34.203.200.174:5002/api/";
 
 export { ipOracle, ipProducts };
