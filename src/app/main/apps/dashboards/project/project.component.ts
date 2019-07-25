6d7cbacaabeef9fd04089622db20a9984dccb08d
ssllumiquinga <ssllumiquinga@espe.edu.ec>
2019-07-24 20:48:48 -0500

    Ventas_Creacion de tareas,reuniones, llamadas, servicios

--- src/app/main/apps/dashboards/project/project.component.ts
+++ src/app/main/apps/dashboards/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import * as shape from 'd3-shape';
 import { ActivatedRoute, Router } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
@@ -15,20 +16,28 @@ import { ScrumboardCardDialogComponent } from 'app/main/apps/scrumboard/board/di
 })
 export class ProjectDashboardComponent implements OnInit
 {
-/*     @Input()
-    list;
-  
-    dialogRef: any; */
+    form: FormGroup;
+    form1: FormGroup;
+    form2: FormGroup;
+    form3: FormGroup;
+    form4: FormGroup;
 
     tasks=[];
     meetings=[];
     communications=[];
     quotations=[];
+    services=[];
     calls=[];
     mails=[];
 
+    /**
+     * Constructor
+     *
+     * @param {FormBuilder} _formBuilder
+     */
     constructor(
         private activateR: ActivatedRoute,
+        private _formBuilder: FormBuilder,
         private _sales: SalesService
         )
     {
@@ -36,6 +45,35 @@ export class ProjectDashboardComponent implements OnInit
 
     ngOnInit(): void
     {
+        this.form = this._formBuilder.group({           
+            description: ['']
+        });
+
+        this.form1 = this._formBuilder.group({
+            topic: [''],
+            description1: [''],
+            date: [''],
+            assistants: [''],
+            duration: [''],
+            place: [''],
+            observation: ['']
+        });
+
+        this.form2 = this._formBuilder.group({
+            subject: [''],
+            description2: [''],
+            date2: [''],
+            observation2: [''],
+            phone: [''],
+            state: ['']
+        });
+        this.form4 = this._formBuilder.group({
+            idService: [''],
+            description4: [''],
+            unitValue: ['']
+        });
+        
+
         this.activateR.params.subscribe(params => {
             const cedula = params['id'];
             this._sales.getTaskBySale(cedula).subscribe(data=>{
@@ -58,25 +96,100 @@ export class ProjectDashboardComponent implements OnInit
                 console.log(data.quotations)
                this.quotations=data.quotations;
             })
+            this._sales.getServices(cedula).subscribe(data=>{
+                console.log(data.services)
+               this.services=data.services;
+            })
             
         });
     }
 
-    /* openCardDialog(cardId): void
-    {
-        this.dialogRef = this._matDialog.open(ScrumboardCardDialogComponent, {
-            panelClass: 'scrumboard-card-dialog',
-            data      : {
-                cardId: cardId,
-                listId: this.list.id
-            }
-        });
-        this.dialogRef.afterClosed()
-            .subscribe(response => {
+    addTask(): void
+    {            
+        console.log("entro por fin");
+        this.activateR.params.subscribe(params => {
+            const cedula = params['id'];
+            console.log(cedula);
+            const data = 
+            {task: {            
+                description: this.form.getRawValue().description,
+                realized: true
+            }};
+            
+            this._sales.postTask(cedula, data).subscribe(data => {
+                console.log(data);
 
             });
-    } */
+        });        
+    }
 
+    addMeeting(): void
+    {            
+        console.log("entro por fin");
+        this.activateR.params.subscribe(params => {
+            const cedula = params['id'];
+            console.log(cedula);
+            const data = 
+            {meeting: {            
+                topic: this.form1.getRawValue().topic,
+                description: this.form1.getRawValue().description1,
+                date: new Date(),
+                assistants: [this.form1.getRawValue().assistants,this.form1.getRawValue().assistants],
+                duration: this.form1.getRawValue().duration,
+                place: this.form1.getRawValue().place,
+                observation: this.form1.getRawValue().observation
+            }};
+            
+            this._sales.postMeeting(cedula, data).subscribe(data => {
+                console.log(data);
+
+            });
+        });        
+    }
+
+    addCall(): void
+    {            
+        console.log("entro por fin");
+        this.activateR.params.subscribe(params => {
+            const cedula = params['id'];
+            console.log(cedula);
+            const data = 
+            {communication: {            
+                subject: this.form2.getRawValue().subject,
+                description: this.form2.getRawValue().description2,
+                date: new Date(),
+                type: "call",
+                place: this.form2.getRawValue().place,
+                observation: this.form2.getRawValue().observation2,
+                call: {
+                    phone: this.form2.getRawValue().phone,
+                    state: this.form2.getRawValue().state
+                }
+            }};
+            
+            this._sales.postCall(cedula, data).subscribe(data => {
+                console.log(data);
+
+            });
+        });        
+    }
+
+    addService(): void
+    {            
+        console.log("entro por fin");
+            const data = 
+            {service: 
+                {            
+                idService: this.form4.getRawValue().idService,
+                description: this.form4.getRawValue().description4,
+                unitValue: this.form4.getRawValue().unitValue
+            }};
+            
+            this._sales.postService(data).subscribe(data => {
+                console.log(data);
+
+            });      
+    }
 }
 
 
--- src/app/main/apps/scrumboard/services/sales.service.ts
+++ src/app/main/apps/scrumboard/services/sales.service.ts
@@ -53,11 +53,51 @@ export class SalesService {
     const url = this._url+'sales/'+id+'/quotations';
     return this.http.get(url);
   }
+  getServices(id: String) :any{
+    const url = this._url+'services';
+    return this.http.get(url);
+  }
+
+
+
   changePhase(id: String, data) :any{
     const headers = new HttpHeaders()
       .set('Content-Type', 'application/json');
       console.log(data);
       const url = this._url+'sales/'+id+'/phase';
+    return this.http.post(url, data, {headers});
+  }
+
+  postTask(id: String, data): any {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+      console.log(data);
+    const url = this._url+'sales/'+id+'/tasks';
+    return this.http.post(url, data, {headers});
+  }
+
+  postMeeting(id: String, data): any {
+    console.log(data);
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+      console.log(data);
+    const url = this._url+'sales/'+id+'/meetings';
+    return this.http.post(url, data, {headers});
+  }
+
+  postCall(id: String, data): any {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+      console.log(data);
+    const url = this._url+'sales/'+id+'/communications';
+    return this.http.post(url, data, {headers});
+  }
+
+  postService(data): any {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+      console.log(data);
+    const url = this._url+'services';
     return this.http.post(url, data, {headers});
   }
 
